bdb24b61e4eb1f5edf7b4d5263f1125fd9e931f7
Ruben Herraiz Parache <rherraiz@cbr7s6.42barcelona.com>
2024-02-09 15:37:32 +0100

    yes

---
+++ Makefile
@@ -0,0 +1,37 @@
+CC				= gcc
+RM				= rm -f
+CFLAGS			= -Wall -Wextra -Werror -I.
+
+NAME = libft.a
+HEADER = libft.h
+
+SRCS = ft_isalnum.c ft_isprint.c ft_memcmp.c\
+	ft_strlcat.c ft_strncmp.c ft_substr.c ft_atoi.c ft_isalpha.c \
+	ft_memcpy.c ft_strchr.c  ft_strlcpy.c \
+	ft_strnstr.c ft_tolower.c ft_bzero.c ft_isascii.c\
+	ft_memmove.c ft_strdup.c  ft_strlen.c  ft_strrchr.c \
+	ft_toupper.c ft_calloc.c  ft_isdigit.c ft_memchr.c  ft_memset.c \
+	ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c \
+	ft_itoa.c ft_strmapi.c ft_strjoin.c ft_strtrim.c ft_striteri.c
+
+OBJS = $(SRCS:.c=.o)
+
+#regla implicita
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+%.o: %.c $(HEADER) Makefile
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+
+fclean:	clean
+	$(RM) $(NAME)
+
+re:	fclean $(NAME)
+
+
+.PHONY:	all, clean, fclean, re
--- ft_split.c
+++ ft_split.c
@@ -6,27 +6,32 @@
 /*   By: rherraiz <rherraiz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:40:38 by rherraiz          #+#    #+#             */
-/*   Updated: 2024/02/09 12:27:09 by rherraiz         ###   ########.fr       */
+/*   Updated: 2024/02/09 15:06:09 by rherraiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static size_t ft_counter(char const *s, char c)
+#include <stdio.h>
+
+static  size_t ft_counter(char const *s, char c)
 {
 	size_t	i;
 	size_t	count;
 
 	i = 0;
-	count = 1;
+	count = 0;
 	if (!s)
 		return (0);
 	while (s[i] != '\0')
 	{
-		if ((s[i] == c && s[i +1] != c) || s[i] == '\0')
+		if ((s[i] == c && s[i + 1] != c) &&  (s[i + 1] != '\0'))
 			count++;
 		i++;
 	}
+	return count;
 }
+
+
 /*
 char	**ft_split(char const *s, char c)
 {
@@ -34,12 +39,12 @@ char	**ft_split(char const *s, char c)
 
 }
 */
-#include <stdio.h>
+
 int main()
 {
-	char const s[] = "Hola que tal";
+	char const s[] = " Hola    que     tal    ";
 	char c = ' ';
 
-	printf("%d", ft_counter(s,c));
-
+	printf("%lu", ft_counter(s,c));
+	return 0;
 }
