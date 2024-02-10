64cd4140f8772898ca2b7928434f5e8456dbd18f
owen <owen@owen.owen>
2024-02-10 11:16:20 +0100

    first2

--- ft_split.c
+++ ft_split.c
@@ -9,7 +9,50 @@
 /*   Updated: 2024/02/03 05:13:35 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+#include <stdio.h>
+
+static size_t	ft_counter(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+	{
+		if ((s[i] != c && s[i + 1] == c) || (s[i] != c && s[i + 1] == '\0'))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static size_t	ft_numstr(const char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
+}
 
 char	**ft_split(char const *s, char c)
 {
+
+
+
 }
+/*
+int main()
+{
+	char const s[] = "  Hola    que     tal    ";
+	char c = ' ';
+	printf("%lu", ft_counter(s,c));
+	return 0;
+}
+*/
